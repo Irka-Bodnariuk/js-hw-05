17bfda7fb302d8d6b64ef924041b29f7a446746b
Iryna Bodnariuk <bodnariuk.iryna1@gmail.com>
2022-09-07 09:41:34 +0300

    second

--- js/comparison-39.js
+++
@@ -1,9 +0,0 @@
-const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
-// Change code below this line
-
-const ascendingReleaseDates = [...releaseDates].sort((a, b) => a - b);
-
-const descendingReleaseDates = [...releaseDates].sort((a, b) => b - a);
-
-console.log(ascendingReleaseDates);
-console.log(descendingReleaseDates);
